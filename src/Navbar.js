401832f67c01b854958fc8743c29d54f85a8d681
vavnilov <vadim.avnilov@gmail.com>
2018-11-08 10:56:13 -0500

    remove signup if user logged in

--- src/Navbar.js
+++ src/Navbar.js
@@ -7,11 +7,11 @@ const Navbar = (props) => {
   return (<header>
     <h1>FoodEase</h1>
       <NavLink exact to="/">Homepage</NavLink>&nbsp;&nbsp;&nbsp;
-      <NavLink to="/signup">Sign Up</NavLink>&nbsp;&nbsp;&nbsp;
       <NavLink to="/search">Leave Review</NavLink>&nbsp;&nbsp;&nbsp;
       <NavLink to="/reviews">See Reviews</NavLink>&nbsp;&nbsp;&nbsp;
       <NavLink to="/follow">Find FoodEasers</NavLink>&nbsp;&nbsp;&nbsp;
-      {localStorage.jwt ? <NavLink to="/" onClick={props.handleLogout}>Logout</NavLink> : <NavLink to="/login">Login</NavLink>}
+      {localStorage.jwt ? <NavLink to="/" onClick={props.handleLogout}>Logout</NavLink> : <NavLink to="/login">Login</NavLink>}&nbsp;&nbsp;&nbsp;
+      {!localStorage.jwt && <NavLink to="/signup">Sign Up</NavLink>}
   </header>)
 };
 
